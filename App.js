5ad3d6b429f04290c22f79d7b37ea628e70354f9
ravi0the0sun <28498241+ravi0the0sun@users.noreply.github.com>
2021-07-21 13:36:46 +1000

    updated README.md and added util

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
 import {create} from 'ipfs-http-client';
+import {inspect} from 'util';
 
 export default function App() {
   const client = create('http://localhost:5001');
@@ -19,11 +20,69 @@ export default function App() {
       console.log(err);
     }
   };
+  const cat = async () => {
+    const CID = 'QmPZ9gcCEpqKTo6aq61g2nXGUhM4iCL3ewB6LDXZCtioEB';
+
+    try {
+      console.log('Demo App .cat start');
+
+      const chunks = [];
+      for await (const chunk of client.cat(CID)) {
+        console.log('Demo App .cat', {chunk, type: typeof chunk});
+        chunks.push(chunk);
+      }
+      const buffer = chunks.reduce((acc, chunk) => [...acc, ...chunk], []);
+      const content = new TextDecoder().decode(new Uint8Array(buffer));
+
+      console.log('Demo App .cat', {content});
+    } catch (error) {
+      console.error('Demo App .cat', {error});
+    }
+  };
+  const IdScreen = () => {
+    const id = async () => {
+      try {
+        console.log('Demo App .id start');
+        console.log('Demo App .id', {result: inspect(await client.id())});
+      } catch (error) {
+        console.error('Demo App .id', {error});
+      }
+    };
+  };
+  const get = async () => {
+    const CID = 'QmfGBRT6BbWJd7yUc2uYdaUZJBbnEFvTqehPFoSMQ6wgdr';
+    try {
+      console.log('Demo App .get start');
+
+      for await (const file of client.get(CID)) {
+        if (!file.content) {
+          continue;
+        }
+
+        const content = [];
+
+        for await (const chunk of file.content) {
+          content.push(chunk);
+        }
+
+        console.log(
+          'Demo App .get',
+          inspect({
+            file,
+            content,
+          }),
+        );
+      }
+    } catch (error) {
+      console.error('Demo App .get', {error});
+    }
+  };
+
   return (
     <>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Hello!</Text>
-        <Button onPress={add} title={'Press Me'} />
+        <Button onPress={get} title={'Press Me'} />
       </View>
     </>
   );
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # README
 
 Demo App for IPFS in react native v0.64.1
+
+Remember to remove these lines if not APFS case sensitive from ios/Podfile
+
+````ruby
+    system('cd Pods/Headers/Public; ln -s Protobuf protobuf')
+    system('cd Pods/OpenSSL-Universal/Frameworks/OpenSSL.xcframework/ios-arm64_arm64e_armv7_armv7s; ln -sfh OpenSSL.framework openssl.framework')
+    system('cd Pods/OpenSSL-Universal/Frameworks/OpenSSL.xcframework/ios-arm64_i386_x86_64-simulator; ln -sfh OpenSSL.framework openssl.framework')
+    system('cd Pods/OpenSSL-Universal/Frameworks/OpenSSL.xcframework/ios-arm64_x86_64-maccatalyst; ln -sfh OpenSSL.framework openssl.framework')
+    system('cd Pods/OpenSSL-Universal/Frameworks/OpenSSL.xcframework/macos-arm64_arm64e_x86_64; ln -sfh OpenSSL.framework openssl.framework')
+    ```
+````
